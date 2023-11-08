ea6c21287520ab5308a94ac11f32a13686335838
Poglia <pedro.poglia@hotmail.com>
2023-11-08 15:37:37 -0300

    feat: CRUD: Create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\Post;
+
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $r)
+    {
+        $newPost = [
+            'title' => 'Meu primeiro post',
+            'content' => ' Conteudo do post',
+            'author' => 'Pedro Poglia'
+        ];
+
+        $post  = new Post($newPost);
+
+        $post->save();
+        dd($post);
+    }
 }
